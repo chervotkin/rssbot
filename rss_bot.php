7351349d91616252896d475cef41b9ba370b9a6a
Alexander Chervotkin <alexander.chervotkin@gmail.com>
2013-07-31 17:58:16 +0400

    Parse Articles list from RSS

--- rss_bot.php
+++ rss_bot.php
@@ -3,14 +3,34 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-define('DRUPAL_ROOT', getcwd());
+define('DRUPAL_ROOT', getcwd().'/..');
+
+if ( !isset( $_SERVER['REMOTE_ADDR'] ) ) {
+  $_SERVER['REMOTE_ADDR'] = '127.0.0.1';
+}
 
 require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 require_once DRUPAL_ROOT . '/phpQuery/phpQuery.php';
 
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 
-// Define RSS feeds list
+// Create Table if not exist
+// $db_query("create table rss_article (hash char(32), src_link text, title text, status int)");
+
+$table_scheme = array(
+    'description' => 'RSS Articles List',
+    'fields' => array(
+        'hash' => array('type' => 'char', 'length' => 32),
+        'src_link' => array('type' => 'text'),
+        'title' => array('type' => 'text'),
+        'status' => array('type' => 'int')
+    )
+);
+if (!db_table_exists('rss_article')) {
+    db_create_table('rss_article', $table_scheme);
+}
+
+//Define RSS feeds list
 //$feeds[] = array("id" => "do", "url" => "http://www.digitaloffroad.com/feed/");
 //$feeds[] = array("id" => "e3", "url" => "http://www.enduro360.com/feed/");
 $feeds[] = array("id" => "e3", url => "test.rss");
@@ -31,8 +51,27 @@ foreach ($feeds as $feed) { // Collect articles
         $link = $p->find("link")->html();
         $date = $p->find("pubDate")->html();
         $stamp = md5($rss_id.$link);
-        print $stamp."\n";
+        $exists = db_query('select * from rss_article where hash = :stamp', array(':stamp' => $stamp));
+        if ($exists->rowCount() > 0) {
+//            print "Article $title already in table\n";
+        } else {
+            db_insert('rss_article')->fields(array(
+                'hash' => $stamp,
+                'src_link' => $link,
+                'title' => $title,
+                'status' => 0
+            )) -> execute();
+        }
         //break;
     }
+    
+    /* TODO:
+     * - get list from rss_articles
+     * - parse page
+     * - download images
+     * - rewrite links
+     * - translate text
+     * - add node to drupal
+     */
 }
 ?>
