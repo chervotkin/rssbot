0315b77e81f82a1abfa4e675d67ba8668c26c4d5
Alexander Chervotkin <alexander.chervotkin@gmail.com>
2013-08-02 18:10:19 +0400

    Added translator

--- rss_bot.php
+++ rss_bot.php
@@ -1,4 +1,5 @@
 <?php
+header('Content-Type: text/html; charset=utf-8');
 /* 
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -9,6 +10,7 @@ if ( !isset( $_SERVER['REMOTE_ADDR'] ) ) {
   $_SERVER['REMOTE_ADDR'] = '127.0.0.1';
 }
 
+require_once 'translator.php';
 require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 require_once DRUPAL_ROOT . '/phpQuery/phpQuery.php';
 
@@ -69,18 +71,19 @@ foreach ($feeds as $feed) { // Collect articles
     
     /* TODO:
      * + get list from rss_articles
-     * - parse page
-     * - download images
-     * - rewrite links
-     * - translate text
-     * - add node to drupal
+     * + parse page
+     * + download images
+     * + rewrite links
+     * + translate text
+     * + add node to drupal
      */
     $articles = db_query("select * from rss_article where status = 0 limit 1");
     foreach ($articles as $article){
         $hash = $article->hash;
         //$html = file_get_contents($article->src_link);
         $html = file_get_contents("test.html");
-        $doc = phpQuery::newDocument($html);
+//        $doc = phpQuery::newDocument($html);
+        $doc = phpQuery::newDocumentFileHTML("test.html", 'utf-8');
         if ($article->rss_id == 'e3') {
             $doc->find("div.yarpp-related")->remove();
             $doc->find("div.addtoany_share_save_container")->remove();
@@ -103,7 +106,57 @@ foreach ($feeds as $feed) { // Collect articles
 		$i->removeAttr('class');
 		$i->removeAttr('onclick');
 	}
-	print $content->html();
+	// Replace anchors
+	$anchors = $img_doc->find("a");
+	foreach ($anchors as $anchor){
+		$i = pq($anchor);
+		$src = $i->attr('href');
+		$ext = pathinfo($src, PATHINFO_EXTENSION);
+		if ((strtolower($ext) == 'jpg') or (strtolower($ext) == 'jpeg')) {
+		    $dst = $img_dir.md5($src).'.'.$ext;
+		    //copy($src, $dst);
+		    $dst = "http://".$_SERVER['REMOTE_ADDR']."/sites/default/files/img/$hash/".md5($src).".".$ext;
+		    $i->attr('href', $dst);
+		    $i->removeAttr('class');
+		    $i->removeAttr('onclick');
+		}
+	}
+	$content->find("style")->remove();
+	
+	$body = utf8_decode($content->html());
+//	$translated = gtranslate($body);
+		
+	print $body;
+	
+	/* ----------------------------------------------
+	** Create Node
+	**-----------------------------------------------
+	*/
+	$node = new stdClass(); // Create a new node object
+	$node->type = "article"; // Or page, or whatever content type you like
+	node_object_prepare($node); // Set some default values
+	// If you update an existing node instead of creating a new one,
+	// comment out the three lines above and uncomment the following:
+	// $node = node_load($nid); // ...where $nid is the node id
+	
+	$node->title    = $article->title;
+	$node->language = LANGUAGE_NONE; // Or e.g. 'en' if locale is enabled
+	
+	$node->uid = 1; // UID of the author of the node; or use $node->name
+	
+	$node->body[$node->language][0]['value']   = $translated;
+	$node->body[$node->language][0]['summary'] = text_summary($translated);
+	$node->body[$node->language][0]['format']  = 'full_html';
+	
+	// I prefer using pathauto, which would override the below path
+	$path = 'node_created_on' . date('YmdHis');
+	$node->path = array('alias' => $path);
+	
+	/*if($node = node_submit($node)) { // Prepare node for saving
+	    node_save($node);
+	    echo "Node with nid " . $node->nid . " saved!\n";
+	}*/
+	
 	//print $imgs;
 /*	if (mkdir($img_dir)){
 
